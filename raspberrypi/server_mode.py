79be1c20503bcf76b9ed0698952db15c427db73c
Jonathan Liu <jonathanhliu21@gmail.com>
2021-07-16 23:00:33 -0700

    Stage 6: Added tracking map

--- raspberrypi/server_mode.py
+++ raspberrypi/server_mode.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, redirect, request
+from copy import deepcopy
 import json
+import os
+
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home_page() -> None:
     return render_template("index.html")
+
 
 @app.route("/cfg", methods=["GET", "POST"])
 def cfg_page() -> None:
@@ -26,21 +31,51 @@ def cfg_page() -> None:
     else:
         return render_template("cfg.html")
 
+
 @app.route("/cfg_saved")
 def cfg_saved_page() -> None:
     return render_template("cfg_saved.html")
+
 
 @app.route("/tzs")
 def tzs_page() -> None:
     return render_template("tzs.html")
 
+
 @app.route("/map")
 def map_page() -> None:
-    return render_template("map.html")
+    def _conv_filename(name):
+        return ("Started tracking at: " + name[0:4]+"-"+name[5:7]+"-"+name[8:10]+" "+name[11:13]+":"+name[14:16]+":"+name[17:19])
+
+    # get all track files
+    filenames = next(os.walk("tracking"), (None, None, []))[2]
+    filenames.sort()
+
+    oldf = deepcopy(filenames)
+    filenames = [_conv_filename(name) for name in filenames]
+    filenames = [filenames, oldf]
+
+    return render_template("map_main.html", filenames=filenames)
+
+@app.route("/map/<f_name>")
+def map_file_page(f_name) -> None:
+    f = open(os.path.join("tracking", f_name), 'r')
+    track_str = f.read()
+    f.close()
+
+    f = open("raspberrypi/cfg.json", 'r')
+    unit = json.load(f)["UNT"]
+
+    return render_template("map.html", tracking=track_str, unit=unit)
+
+# @app.route("/static/<img>", methods=["GET"])
+# def get_img(img):
+#     pass
+
 
 def main() -> None:
-    app.run(host = "0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 if (__name__ == "__main__"):
     main()
-
--- raspberrypi/static/js/map.js
+++ raspberrypi/static/js/map.js
@@ -1,12 +1,146 @@
-var mymap = L.map('mapid').setView(['39.7392', '104.9903'], 13);
+var tot_dist = 0;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/jonyboi396825/ckr5pbzyq1n4t17mo8g4ykvr8.html?fresh=true&title=view&access_token=pk.eyJ1Ijoiam9ueWJvaTM5NjgyNSIsImEiOiJja3F2cjBmZ2gwaDVtMnZ0Zm9nNmtmd2xiIn0.1imQCah8Gzp6HP_EJKKgmA', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+function get_dist_lat_lon(s1, s2){
+    // get coordinates from strings
+    const cor1 = s1.trim().split(",");
+    const cor2 = s2.trim().split(",");
 
-// mapbox://styles/jonyboi396825/ckr5pbzyq1n4t17mo8g4ykvr8
+    // parse coordinates as floats
+    const la1 = parseFloat(cor1[0]);
+    const lo1 = parseFloat(cor1[1]);
+    const la2 = parseFloat(cor2[0]);
+    const lo2 = parseFloat(cor2[1]);
+
+    // uses the haversine formula and returns in meters
+    // code below and formula info from http://www.movable-type.co.uk/scripts/latlong.html
+
+    const R = 6371e3; // radius of earth in meters
+    const p1 = la1*Math.PI/180;
+    const p2 = la2*Math.PI/180;
+    const dp = (la2-la1)*Math.PI/180;
+    const dl = (lo2-lo1)*Math.PI/180;
+
+    const a = Math.sin(dp/2)*Math.sin(dp/2) + Math.cos(p1)*Math.cos(p2)*Math.sin(dl/2)*Math.sin(dl/2);
+    const c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R*c;
+
+    return d;
+}
+
+function calc_tot_dist(arr){
+    var dist = 0;
+    var prev_cor = arr[0];
+    for (let i = 0; i < arr.length; i++){
+        let cur_cor = arr[i];
+        if (cur_cor == "PAUSED" && i === arr.length-1) break;
+
+        // console.log("before", i, cur_cor, prev_cor)
+        if (cur_cor === "PAUSED" && i !== arr.length-1){
+            prev_cor = arr[i+1];
+            continue;
+        } else{
+            dist += get_dist_lat_lon(cur_cor, prev_cor);
+        }
+        // console.log(i, cur_cor, prev_cor, dist, get_dist_lat_lon(cur_cor, prev_cor));
+
+        prev_cor = cur_cor;
+    }
+
+    return dist;
+}
+
+function conv_dist_unit(dist, unit){
+    if (unit === "0"){
+        return (dist/1609).toFixed(6);
+    } else{
+        return (dist/1000).toFixed(3);
+    }
+}
+
+function handle_track_data(s, unit){
+    var arr = s.trimEnd().split("\n");
+    var dist = calc_tot_dist(arr);
+
+    var start_cor = arr[0];
+    var end_cor = arr[arr.length-1];
+
+    var mymap = L.map('mapid').setView(start_cor.trim().split(","), 15);
+
+    L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19,
+        tileSize: 512,
+        zoomOffset: -1,
+        detectRetina: true
+    }).addTo(mymap); 
+
+    L.marker(start_cor.trim().split(","), {
+        title: 'start',
+        icon: new L.Icon({
+            iconUrl: "/static/img/start.png",
+            iconSize: 40
+        })
+    }).addTo(mymap);
+
+    L.marker(end_cor.trim().split(","), {
+        title: 'end',
+        icon: new L.Icon({
+            iconUrl: "/static/img/end.png",
+            iconSize: 40
+        })
+    }).addTo(mymap);
+
+    var ptr = 0; 
+    while (ptr < arr.length){
+        let coords = [];
+        while (ptr < arr.length && arr[ptr] !== "PAUSED"){
+            coords.push(arr[ptr].trim().split(","));
+            ptr++;
+        }
+
+        L.polyline(coords, {
+            color: "red",
+            smoothFactor: "4.0"
+        }).addTo(mymap);
+
+        let s1 = start_cor.trim().split(",");
+        let s2 = end_cor.trim().split(",");
+        if (coords[0][0] !== s1[0] || coords[0][1] !== s1[1]){
+            L.marker(coords[0], {
+                title: 'resumed',
+                icon: new L.Icon({
+                    iconUrl: "/static/img/resume.png",
+                    iconSize: 40
+                })
+            }).addTo(mymap);
+        }
+
+        if (coords[coords.length-1][0] !== s2[0] || coords[coords.length-1][1] !== s2[1]){
+            L.marker(coords[coords.length-1], {
+                title: 'paused',
+                icon: new L.Icon({
+                    iconUrl: "/static/img/pause.png",
+                    iconSize: 40
+                })
+            }).addTo(mymap);
+        }
+
+        while (ptr < arr.length && arr[ptr] === "PAUSED"){
+            ptr++;
+        }
+    }
+
+    var unit_str = (unit == 0 ? "mi" : "km");
+    var dist_str = "Total distance: ".concat(String(conv_dist_unit(dist, unit)), unit_str)
+
+    new Vue({
+        el: "#dist",
+        delimiters: ["[[", "]]"],
+        data: {
+            distance: dist_str
+        }
+    });
+}
+
+
