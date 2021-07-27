7e74c13babcba751f867f8cfec219b564d8e9050
Jonathan Liu <jonathanhliu21@gmail.com>
2021-07-27 14:04:17 -0700

    init venv when running

--- run.bash
+++ run.bash
@@ -2,9 +2,15 @@
 
 set -e;
 
-# run the python program
+# cd into directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-
 cd $DIR;
+
+# init venv
+source env/bin/activate
+
+# start python program
 python3 .;
 
+# deactivate venv if out of program
+deactivate;
